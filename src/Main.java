ac4fb5591e982572255a5e67dec4d2496d12f256
Lenka <aruru.goddes@gmail.com>
2023-06-15 20:41:21 +0300

    Выполнена корректировка домашнего задания №7 по замечаниям от 15.06.2023

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+    }
+    public static void task1() {
         //Задача 1
         System.out.println("Задача 1");
         int storage1 = 15000;
@@ -8,8 +18,10 @@ public class Main {
         while (total1 < 2_459_000) {
             total1 = total1 + storage1;
             month = month + 1;
-            System.out.println("Месяц " + month + ", сумма накоплений равна " + total1 + " рублей");
         }
+        System.out.println("Месяц " + month + ", сумма накоплений равна " + total1 + " рублей");
+    }
+    public static void task2() {
         //Задача 2
         System.out.println("Задача 2");
         int number = 1;
@@ -23,6 +35,8 @@ public class Main {
             System.out.print(number + " ");
         }
         System.out.println();
+    }
+    public static void task3() {
         //Задача 3
         System.out.println("Задача 3");
         float totalPeople = 12_000_000;
@@ -30,24 +44,28 @@ public class Main {
         float deathPeople = 8 / 1000f;
         for (int i = 1; i <= 10; i++) {
             totalPeople = totalPeople + totalPeople * bornPeople - totalPeople * deathPeople;
-            System.out.println("Год " + i + "й, численность населения составляет " + totalPeople + " человек");
+            System.out.println("Год " + i + "й, численность населения составляет " + Math.round(totalPeople) + " человек");
         }
+    }
+    public static void task4() {
         //Задача 4
         System.out.println("Задача 4");
         int storage4 = 15000;
         float total4 = 0;
-        float percent4 = 7/100f;
+        float percent4 = 7 / 100f;
         int goal4 = 12_000_000;
         for (int i = 1; total4 < goal4; i++) {
             total4 = total4 + total4 * percent4;
             total4 = total4 + storage4;
             System.out.println("Месяц " + i + ", сумма накоплений равна " + total4 + " рублей");
         }
+    }
+    public static void task5() {
         //Задача 5
         System.out.println("Задача 5");
         int storage5 = 15000;
         float total5 = 0f;
-        float percent5 = 7/100f;
+        float percent5 = 7 / 100f;
         int goal5 = 12_000_000;
         for (int i = 1; total5 < goal5; i++) {
             total5 = total5 + total5 * percent5;
@@ -56,11 +74,13 @@ public class Main {
                 System.out.println("Месяц " + i + ", сумма накоплений равна " + total5 + " рублей");
             }
         }
+    }
+    public static void task6() {
         //Задача 6
         System.out.println("Задача 6");
         int storage6 = 15000;
         float total6 = 0f;
-        float percent6 = 7/100f;
+        float percent6 = 7 / 100f;
         int period = 9 * 12;
         for (int i = 1; i <= period; i++) {
             total6 = total6 + total6 * percent6;
@@ -69,6 +89,8 @@ public class Main {
                 System.out.println("Месяц " + i + ", сумма накоплений равна " + total6 + " рублей");
             }
         }
+    }
+    public static void task7() {
         //Задача 7
         System.out.println("Задача 7");
         int friday = 4;
@@ -78,6 +100,8 @@ public class Main {
                 friday = friday + 7;
             }
         }
+    }
+    public static void task8() {
         //Задача 8
         System.out.println("Задача 8");
         int year = 2023;
